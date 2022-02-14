f3ebcdfc8b74df860604075e38e1b8168563f157
AndyC <djandycampbell@googlemail.com>
2022-02-14 00:11:47 +0000

    Added Posts page && 2 dummy data rows

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link, useLoaderData } from "remix";
+
+export type Post = {
+  slug: string;
+  title: string;
+};
+
+export const loader = async () => {
+  const posts: Post[] = [
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just For You",
+    },
+  ];
+  return posts;
+};
+
+const Posts = () => {
+  const posts = useLoaderData<Post[]>();
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Posts;
